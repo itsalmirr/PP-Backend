83f9a09f3aa6f2370573a5f433d45bba201c8400
almirr <itsalmirr@icloud.com>
2024-12-09 08:12:13 -0500

    setup for supabase DB

--- src/cmd/db.go
+++ src/cmd/db.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,11 +12,25 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := ""
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load .env file!")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
 
+	fmt.Println("Database connected!")
 	DB = database
 }
