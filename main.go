2f11fedb7c8a9871634afc3fb3b1273533d774ad
almirr <itsalmirr@icloud.com>
2024-12-09 07:12:59 -0500

    changed file structure

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"backend.com/go-backend/src/models"
+	"backend.com/go-backend/src/cmd"
 	"backend.com/go-backend/src/routers"
 )
 
 func main() {
-	models.ConnectDatabase()
+	cmd.ConnectDatabase()
 
 	router := routers.SetupRouter()
 	router.Run(":8080")
--- src/cmd/db.go
+++ src/cmd/db.go
@@ -1,4 +1,4 @@
-package models
+package cmd
 
 import (
 	"gorm.io/driver/postgres"
