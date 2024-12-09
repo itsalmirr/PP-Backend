47f883cef2915e052478d49302f8da947e065c68
almirr <itsalmirr@icloud.com>
2024-12-09 07:09:49 -0500

    added db

---
+++ src/models/db.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := ""
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	DB = database
+}
