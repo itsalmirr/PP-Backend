e2d061034eb51adf31df0fe2dd0c7a15c5c9c1a2
almirr <itsalmirr@icloud.com>
2024-12-09 07:10:28 -0500

    added db

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"backend.com/go-backend/src/models"
 	"backend.com/go-backend/src/routers"
 )
 
 func main() {
+	models.ConnectDatabase()
+
 	router := routers.SetupRouter()
 	router.Run(":8080")
 }
