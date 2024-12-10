48caed54d1f9af883f09d2d5fca4a6d2fe48b309
almirr <itsalmirr@icloud.com>
2024-12-10 05:32:47 -0500

    added oauth2.0

--- src/routers/router.go
+++ src/routers/router.go
@@ -3,9 +3,32 @@ package routers
 import (
 	"backend.com/go-backend/src/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	ginoauth2 "github.com/zalando/gin-oauth2"
+
+	"github.com/zalando/gin-oauth2/google"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+
+	"os"
 )
 
 func SetupRouter() *gin.Engine {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load .env file!")
+	}
+
+	// OAuth2 configuration
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes:       []string{"profile", "email"},
+		Endpoint:     google.Endpoint,
+		RedirectURL:  "http://localhost:8080/auth/callback",
+	}
+
 	router := gin.Default()
 
 	// Public routes
@@ -17,6 +40,15 @@ func SetupRouter() *gin.Engine {
 			userRoutes.POST("/", controllers.CreateUser)
 			userRoutes.GET("/:username", controllers.GetUser)
 		}
+	}
+
+	// Protected routes with OAuth2 middleware
+	protected := router.Group("/api/v1")
+	protected.Use(ginoauth2.Auth(conf))
+	{
+		protected.GET("/protected", func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "You are authorized"})
+		})
 	}
 
 	return router
